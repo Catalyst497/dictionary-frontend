6db1276cba59c31bdd5b43e04c51788367c73ea9
Catalyst <danielduyilemi@gmail.com>
2024-01-04 10:59:50 +0100

    Add API link.

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server:  {
     proxy: {
       '/api': {
-          target: 'http://localhost:3001/',
+          target: 'https://dictionary-backend-ya1b.onrender.com',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
       },
